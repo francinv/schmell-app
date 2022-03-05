ca45f75b0fe43392a38da2127c088d2391cf0b5c
Francin Vincent <70270809+francinv@users.noreply.github.com>
2022-03-05 23:22:28 +0100

    Revert ":sparkles: Redux"

--- schmellApp/App.tsx
+++ schmellApp/App.tsx
@@ -73,7 +73,7 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Pravine r gay">
+          <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
