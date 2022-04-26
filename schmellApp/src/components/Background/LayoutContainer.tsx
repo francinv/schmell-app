632e301cdee773ebedfe1ea10a767929fb200e98
Francin Anoj Vincent <francinv@stud.ntnu.no>
2022-04-26 11:16:32 +0200

    :sparkles: (#8) Game Overview
    Completed background animation

--- schmellApp/src/components/Background/LayoutContainer.tsx
+++ schmellApp/src/components/Background/LayoutContainer.tsx
@@ -1,12 +1,42 @@
-import React from 'react';
-import LinearGradient from 'react-native-linear-gradient';
+import React, {useEffect, useState} from 'react';
+import {Animated} from 'react-native';
 import globalStyles from '../../styles/global.styles';
 
 const LayoutContainer: React.FC = ({children}) => {
+  const [animation] = useState(new Animated.Value(0));
+
+  const handleAnimation = () => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration: 3500,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 3500,
+        useNativeDriver: false,
+      }).start(() => handleAnimation());
+    });
+  };
+
+  const boxInterpolation = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['rgb(0, 226, 191)', 'rgb(110, 82, 143)'],
+  });
+
+  const animatedStyle = {
+    backgroundColor: boxInterpolation,
+  };
+
+  useEffect(() => {
+    handleAnimation();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <LinearGradient colors={['#1C0C5B', '#3D2C8D']} style={globalStyles.flex_1}>
+    <Animated.View style={[globalStyles.flex_1, animatedStyle]}>
       {children}
-    </LinearGradient>
+    </Animated.View>
   );
 };
 
