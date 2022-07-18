869ce5fa272fabdac187985a1d9e0f3bf4f7c24b
Francin Vincent <francin.vinc@gmail.com>
2022-07-18 10:55:37 +0200

    :art: Remove unused method

--- schmellApp/src/utils/filterMethods.ts
+++ schmellApp/src/utils/filterMethods.ts
@@ -1,27 +1,5 @@
-import {gameType} from '../typings/gameTypes';
 import {questionType} from '../typings/questionTypes';
-import {weekType} from '../typings/weekTypes';
 import _ from 'lodash';
 
-export function filterList(
-  currentWeek: number,
-  weeks: weekType[],
-  selectedGame: gameType,
-  questions: questionType[],
-) {
-  var filteredByGameArray = questions.filter(function (value) {
-    return value.related_game === selectedGame.id;
-  });
-  let wantedWeek = weeks?.find(function (value) {
-    return value.game === selectedGame.id && value.week_number === currentWeek;
-  });
-  var filteredArray = filteredByGameArray?.filter(function (value) {
-    return value.related_week === wantedWeek?.id;
-  });
-  return filteredArray;
-}
-
-export function randomizeList(listToRandomize: any) {
-  let randomizedList = _.shuffle(listToRandomize);
-  return randomizedList;
-}
+export const randomizeList = (listToRandomize: questionType[]) =>
+  _.shuffle(listToRandomize);
