74f386eb56215442d384343ecec981b132836f9d
Francin Vincent <francin.vinc@gmail.com>
2022-07-21 23:24:35 +0200

    :recycle: lint code

--- schmellApp/src/utils/dateUtil.ts
+++ schmellApp/src/utils/dateUtil.ts
@@ -2,9 +2,6 @@ export const getCurrentWeekNumber = (): number => {
   let d = new Date();
   d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
   d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  var weekNo = Math.ceil(
-    ((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7,
-  );
-  return weekNo;
+  let yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil(((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7);
 };
