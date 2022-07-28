5127c88e7f1e5b61a5b75e4ff8c58b8e7f51c58a
Francin Vincent <francin.vinc@gmail.com>
2022-07-29 01:41:51 +0200

    :recycle: Removed console.logs

--- schmellApp/src/native/RNLockOrientation.ts
+++ schmellApp/src/native/RNLockOrientation.ts
@@ -2,11 +2,9 @@ import {NativeModules} from 'react-native';
 const {RNLockOrientation} = NativeModules;
 
 export const lockPortrait = () => {
-  console.log('portrait triggered');
   RNLockOrientation.lockToPortrait();
 };
 
 export const lockToLandscape = () => {
-  console.log('landscape triggered');
   RNLockOrientation.lockToLandscapeLeft();
 };
