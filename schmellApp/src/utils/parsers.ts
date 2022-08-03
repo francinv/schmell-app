e5efe7edc69193c1123168d128e074c613aacb23
Francin Vincent <francin.vinc@gmail.com>
2022-08-03 22:14:08 +0200

    :construction: Created parser method for countdown

--- schmellApp/src/utils/parsers.ts
+++ schmellApp/src/utils/parsers.ts
@@ -1,2 +1,5 @@
 export const parseFunction = (questionFunc: string) =>
   JSON.parse(questionFunc).answer;
+
+export const parseFunctionTimer = (questionFunc: string) =>
+  questionFunc ? JSON.parse(questionFunc).timer : undefined;
